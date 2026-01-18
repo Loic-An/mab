d32b5f95c7fb5ae68f576224285d2fad2872a99e
Loic André <loic.andre00@gmail.com>
2026-01-18 21:43:12 +0100

    remove crap

--- inc/vl53l0x_types.h
+++
@@ -1,8 +0,0 @@
-#ifndef VL53L0X_TYPES_H_
-#define VL53L0X_TYPES_H_
-#include <stdint.h>
-#include <stddef.h>
-typedef int8_t VL53L0X_Error;
-typedef uint8_t VL53L0X_DeviceState;
-typedef int32_t FixPoint1616_t;
-#endif
