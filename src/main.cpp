9ee01cae07ba6e108394118cc49ede56e4fd73d2
Losheler <reynaudmaxenceldlf@gmail.com>
2026-01-22 10:08:46 +0100

    offset v5

--- src/main.cpp
+++ src/main.cpp
@@ -112,8 +112,8 @@ int test_vl53l0x()
         fprintf(stderr, "Erreur mesure distance VL53L0X\n");
         return EXIT_FAILURE;
     }
-    if(distance < 80) { distance -= 10;}
-    else if(distance > 80) {distance += 10;}
+    if(distance < 80) { distance += 10;}
+    else if(distance > 80) {distance -= 10;}
     printf("Distance mesurée : %u mm\n", distance);
     return EXIT_SUCCESS;
 }
